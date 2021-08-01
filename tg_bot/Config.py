91ea1a34f1f910c0b466459fa3db7af2562f14d7
sjflow17 <82510862+sjflow17@users.noreply.github.com>
2021-08-01 10:42:54 +0530

    Update Config.py

--- tg_bot/Config.py
+++ tg_bot/Config.py
@@ -3,7 +3,7 @@ from tg_bot.sample_config import Config
 
 class Development(Config):
     OWNER_ID = 1795893951  # my telegram ID
-    OWNER_USERNAME = "Kindaoulforlife"  # my telegram username
+    OWNER_USERNAME = "Kindsoulforlife"  # my telegram username
     API_KEY = "1762928259:AAHIB6951EGlnZ-YMGZlkMxgky-EPub_dcg"  # my api key, as provided by the botfather
     SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/database'  # sample db credentials
     MESSAGE_DUMP = '-1001230732953' # some group chat that your bot is a member of
